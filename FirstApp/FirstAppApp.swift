27410a2813e1c4ab35731eb6b5ef1c6f3fc004f0
Tony Tran <qntbkk@gmail.com>
2021-06-12 12:46:54 +0700

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Tony Tran on 6/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
